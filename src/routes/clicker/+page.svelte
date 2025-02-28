500edfed32eaa1779d06ed8d33bf333c5260e3ff
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2025-02-27 16:54:48 -0800

    Update clicker app

--- src/routes/clicker/+page.svelte
+++ src/routes/clicker/+page.svelte
@@ -4,4 +4,23 @@
 
 <h1>Clicker</h1>
 
-<button on:click={() => count++}>The count is {count}</button>
+<button class="cookie-button" on:click={() => count++}>
+	<img
+		class="cookie-image"
+		src="https://placecats.com/100/100"
+		alt="Cookie Cat from Steven Universe"
+	/>
+
+	The count is {count}</button
+>
+
+<style>
+	.cookie-button {
+		border: none;
+	}
+
+	.cookie-image {
+		width: 100px;
+		height: 100px;
+	}
+</style>
